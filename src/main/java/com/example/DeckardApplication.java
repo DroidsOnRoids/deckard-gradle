6de13a477d3b3a832bd924fd1163d482cbdaa7a3
koral <koral>
2015-02-03 02:37:49 +0100

    more demos added

--- src/main/java/com/example/DeckardApplication.java
+++ src/main/java/com/example/DeckardApplication.java
@@ -4,4 +4,13 @@ import android.app.Application;
 
 public class DeckardApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
 }
--- src/test/java/com/example/activity/DeckardActivityTest.java
+++ src/test/java/com/example/activity/DeckardActivityTest.java
@@ -1,17 +1,81 @@
 package com.example.activity;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class DeckardActivityTest {
 
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
     @Test
-    public void testSomething() throws Exception {
+    public void testPass() throws Exception {
         assertTrue(Robolectric.buildActivity(DeckardActivity.class).create().get() != null);
+    }
+
+    @Test
+    public void testPassAsserts() throws Exception {
+        final DeckardActivity deckardActivity = Robolectric.buildActivity(DeckardActivity.class).create().get();
+        assertTrue(deckardActivity != null);
+        assertNotNull(deckardActivity);
+        int x = 2 + 2;
+        int y = 3 + 1;
+        assertEquals(x, y);
+
+        assertThat(deckardActivity).isNotNull();
+        assertThat(1).isIn(Arrays.asList(1, 3, 5)).isBetween(0, 2);
+        Date now = new Date();
+        assertThat(now).isAfterYear(2000);
+        assertThat(deckardActivity.getFilesDir()).exists();
+        assertThat("\uD83D\uDE39").hasSameSizeAs("\uD83D\uDE38");
+    }
+
+    @Test
+    public void testFail() throws Exception {
+        assertThat(new Date()).isCloseTo(new Date(), -1);
+    }
+
+    @Test
+    public void testError() throws Exception {
+        int x = new int[-1].length;
+    }
+
+    @Ignore
+    @Test
+    public void testIgnore() throws Exception {
+        notify();
     }
 }
